f882010dacd686d16af3c34be15b29d779ee8ccf
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-24 22:56:33 -0400

    this is the timer fix

--- script.js
+++ script.js
@@ -80,32 +80,32 @@ $(document).ready(function () {
 
 });
 // Set the date we're counting down to
-// var countDownDate = new Date();
-// countDownDate.setDate(countDownDate.getDate() + 10);
+var countDownDate = new Date();
+countDownDate.setHours(countDownDate.getHours() + 1);
+countDownDate = countDownDate.getTime()
 
 // // Update the count down every 1 second
-// var x = setInterval(function () {
+ var x = setInterval(function () {
 
 //     // Get today's date and time
-//     var now = new Date().getTime();
+     var now =  Date.now();
 
 //     // Find the distance between now and the count down date
-//     var distance = countDownDate - now;
+     var distance = countDownDate - now;
 
 //     // Time calculations for days, hours, minutes and seconds
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-//     $(".sale-timer__days").prepend(days);
-//     $(".sale-timer__hours").prepend(hours);
-//     $(".sale-timer__minutes").prepend(minutes);
-//     $(".sale-timer__seconds").prepend(seconds);
+     $(".sale-timer__hours").prepend(hours);
+     $(".sale-timer__minutes").prepend(minutes);
+     $(".sale-timer__seconds").prepend(seconds);
 
 //     // If the count down is over, write some text 
-//     if (distance < 0) {
-//         clearInterval(x);
-//         document.getElementById("countdownSale").innerHTML = "EXPIRED";
-//     }
-// }, 1000);
+     if (distance < 0) {
+         clearInterval(x);
+         document.getElementById("countdownSale").innerHTML = "DELIVERED";
+     }
+ }, 1000);
