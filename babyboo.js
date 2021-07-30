b4bf3b6ff1f9961b5c8741422e39dcddffee6bf1
1brahimsaid <ibrahim.said95@gmail.com>
2021-07-30 11:16:44 -0400

    return to patch1

--- babyboo.js
+++ babyboo.js
@@ -1,9 +1,33 @@
+const { ethers } = require("ethers");
+
+
+let window;
 let accounts;
 let contract;
 let account;
 let subscription;
-
 let total;
+let provider;
+
+const babyBooAddress = "0x471762A7017A5B1A3e931F1A97aa03Ef1E7F4A73";
+const abi = [
+  "function claim() external"
+];
+
+const babyBoo = new ethers.Contract(babyBooAddress, abi, provider);
+
+const initializeEthers = async () => {
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+  try {
+    const signer = provider.getSigner(0);
+    const addr = await signer.getAddress();
+    const checkNet = (window.ethereum.networkVersion === "250");
+  } catch (error) {
+    return;
+  }
+}
+
+await initializeEthers();
 
 window.addEventListener('load', async function() {
     Notiflix.Notify.Init({
@@ -39,19 +63,21 @@ window.addEventListener('load', async function() {
     }, 1000);
 })
 
+const babyBoo = new ethers.Contract(babyBooAddress, abi, provider);
 
-function claimBOO() {
-    console.log("Trying to Claim...")
-    contract.methods.claim().call({from: account}, (err, result) => {
-        console.log("calling claim...")
-        if (err) {
-            alert("Nothing To Claim, Please wait to earn more BOO!");
-            return;
-        }
-})
-
+async function claimBOO() {
+  try {
+    const tx = await babyBoo.claim();
+    const receipt = await tx.wait();
+    if (receipt.status === 0) {
+      throw new Error("Transaction failed.");
+    }
+  } catch (error) {
+    if (error.code === 4001) {
+      return;
+    }
+  }
 }
-
 
 async function updateData() {
     account = accounts[0];
@@ -59,36 +85,4 @@ async function updateData() {
     if (ethereum.chainId != "0xfa") {
         alert("Switch to Fantom Opera Mainnet");
     }
-
-    // contract.methods.tokensToClaim(account).call({from: account}, (err, result) => {
-    //     if (err) {
-    //         document.getElementById('tokensToClaim').innerHTML = "0 BabyBOO"; // user account not created! so invalid opcode!
-    //         return;
-    //     }
-        
-    //     total = result;
-    //     contract.methods.balanceOf(account).call({from: account}, (err, result) => {
-    //         if (((total - result) / 1e6 ) < 0 ){
-    //             document.getElementById('tokensToClaim').innerHTML = "0 BabyBOO";
-    //         } else {
-    //             document.getElementById('tokensToClaim').innerHTML = (total - result) / 1e6 + " BabyBOO";
-    //         }
-    //     });
-    // });
-
-}
-
-// function claimBOO() {
-//     if (tokensToClaim.innerHTML != "0 BabyBOO") {
-//         contract.methods.transfer(account, 0).send({ 
-//             from: account, 
-//             value: 0
-//         }).on('receipt', function(receipt) {
-//             console.log(receipt);
-//             Notiflix.Notify.Success('Successfully claimed BOO!');
-//         });
-//         Notiflix.Notify.Success('Sending transaction...');
-//     } else {
-//         alert("Nothing To Claim, Please wait to earn more BOO");
-//     }
-// }
+  }
